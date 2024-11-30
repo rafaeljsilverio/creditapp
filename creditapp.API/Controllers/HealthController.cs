e7dda95fe4afa48555318a7e9dd5c9d74543bfab
Rafael Silverio <rafael.silverio@coxautoinc.com>
2024-11-29 20:40:39 -0400

    Add project files.

---
+++ creditapp.API/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace creditapp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Healthy");
+        }
+    }
+}
